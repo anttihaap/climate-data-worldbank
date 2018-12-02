dab0c527cf6bf2b903ceb63cfed702136cc6c10c
anttihaap <antti.haapaniemi@helsinki.fi>
2018-12-03 00:21:07 +0200

    Fix css overlay issue with for search suggestions

--- client/src/components/compareCountries/CountryAdd.js
+++ client/src/components/compareCountries/CountryAdd.js
@@ -42,6 +42,17 @@ class CountryAdd extends React.Component {
             value={this.state.value}
             onChange={e => this.setState({ value: e.target.value })}
             onSelect={value => this.setState({ value })}
+            menuStyle={{
+              borderRadius: "3px",
+              boxShadow: "0 2px 12px rgba(0, 0, 0, 0.1)",
+              background: "rgba(255, 255, 255, 0.9)",
+              padding: "2px 0",
+              fontSize: "90%",
+              position: "fixed",
+              zIndex: 9001,
+              overflow: "auto",
+              maxHeight: "50%"
+            }}
           />
 
           <p />
